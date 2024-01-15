a1c5e1833821eb9cfb3f78a765a536f94c9e0c74
MuhammadHassan24 <hassandarwaish0@gmail.com>
2024-01-15 13:21:04 +0500

    todo app task

---
+++ lib/screens/profile/profileview.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:stacked/stacked.dart';
+import 'package:todoapp/screens/profile/profileviewmodel.dart';
+
+class ProfileView extends StatelessWidget {
+  const ProfileView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ViewModelBuilder.reactive(
+        viewModelBuilder: () => ProfileViewModel(),
+        builder: (context, viewModel, child) {
+          return Scaffold();
+        });
+  }
+}
